7b1c9223516ba4551be7af94812d42efa0242b16
Matuzo <MatuzoCaos@gmail.com>
2022-04-14 21:58:23 -0300

    :recycle: refactor: Refatorando primeira versão do aquisição de dados

--- AquisicaoDosDados.py
+++ AquisicaoDosDados.py
@@ -12,14 +12,9 @@ import glob
 import os
 import numpy as np
 
-# Pasta para salvar as imagens dos captchas obtidos
-PASTA_CAPTCHAS = ".\\CaptchasOriginais"
-
-# Pasta para salvar as imagens dos captchas processados
-PASTA_CAPTCHAS_PROCESSADOS = ".\\CaptchasProcessados"
-
-# Pasta para salvar as imagens dos caracteres extraidos dos captchas processados
-PASTA_CARACTERES = ".\\Caracteres"
+PASTA_CAPTCHAS = ".\\CaptchasOriginais"                  # Pasta para salvar as imagens dos captchas obtidos
+PASTA_CAPTCHAS_PROCESSADOS = ".\\CaptchasProcessados"    # Pasta para salvar as imagens dos captchas processados
+PASTA_CARACTERES = ".\\Caracteres"                       # Pasta para salvar as imagens dos caracteres extraidos dos captchas processados
 
 # Metodo para acessar o site da UVT RN e adquirir imagens dos captchas
 # O usuario devera inserir a solução de cada captcha para que o nome da imagem seja a solução
@@ -36,8 +31,7 @@ def PegarCaptchas():
     # Adquire captchas ate que o usuario digite sair
     while True:
 
-        # Acessa a pagina de login
-        driver.find_element_by_id("codeaccess-btn").click()
+        driver.find_element_by_id("codeaccess-btn").click()     # Acessa a pagina de login
 
         # Aguarda pagina de login da UVT RN carregar
         try:
@@ -56,6 +50,7 @@ def PegarCaptchas():
         # Aguarda o usuario digitar a solução do captcha que sera o nome do arquivo imagem
         # O usuario tambem podera digitar alguns comandos para realizar outros comportamentos no algoritmo
         # Ex. "pular" para pular esse captcha
+        print("")
         print("Digite o codigo do captcha ou um dos seguintes comandos:")
         print("pular : o algoritmo ignora o captcha atual e vai para o proximo")
         print("sair : o algoritmo ignora o captcha atual e é encerrado")
@@ -65,8 +60,7 @@ def PegarCaptchas():
 
             print("Pulando o captcha atual...")
 
-            # Atualiza a pagina
-            driver.refresh()
+            driver.refresh()    # Atualiza a pagina
             
             # Aguarda a pagina carregar
             try:
@@ -88,17 +82,16 @@ def PegarCaptchas():
             while(len(teste)!=5):
                 print("O captcha tem obrigatoriamente 5 caracteres, por favor insira novamente")
                 nome = input("Digite : ")
+                nome = nome.upper()
                 teste = list(nome)
 
-            # Salva a imagem com a solução descrita pelo usuario
-            urllib.request.urlretrieve(src, PASTA_CAPTCHAS + "\\" + str(nome) + ".png")
+            urllib.request.urlretrieve(src, PASTA_CAPTCHAS + "\\" + str(nome) + ".png")  # Salva a imagem com a solução descrita pelo usuario
 
             numero_de_captchas_coletados += 1
             print(numero_de_captchas_coletados, " captchas coletados")
             print("")
 
-            # Atualiza a pagina
-            driver.refresh()
+            driver.refresh()    # Atualiza a pagina
             
             # Aguarda a pagina carregar
             try:
@@ -116,13 +109,14 @@ def ProcessarCaptchas():
 
     print("Processamento de imagens iniciado")
 
-    # Para cada arquivo de imagem na pasta dos captchas
-    image_files = glob.glob(os.path.join(PASTA_CAPTCHAS, "*"))
+    image_files = glob.glob(os.path.join(PASTA_CAPTCHAS, "*"))  # Para cada arquivo de imagem na pasta dos captchas
 
     for (i, captcha_file) in enumerate(image_files):
 
         # Adquire o nome do arquivo do captcha
         nome_arquivo = captcha_file.split("\\")[2]
+        nome_puro_arquivo = nome_arquivo.split(".")[0]
+        caracteres = list(nome_puro_arquivo)
         
         # Abre a imagem e aplica processamento
         img = cv2.imread(captcha_file)    
@@ -130,64 +124,30 @@ def ProcessarCaptchas():
         img_processada = cv2.cvtColor(img_processada, cv2.COLOR_BGR2GRAY)
         _, img_processada = cv2.threshold(img_processada, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
         img_processada = cv2.bitwise_not(img_processada)
-        
-        nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(img_processada, connectivity=8)
-        sizes = stats[1:, -1]; nb_components = nb_components - 1
-        min_size = 50
-        img_processada2 = np.zeros((output.shape))
-        for i in range(0, nb_components):
-            if sizes[i] >= min_size:
-                img_processada2[output == i + 1] = 255
-
-        cv2.imwrite(PASTA_CAPTCHAS_PROCESSADOS + "//" + nome_arquivo, img_processada2) # Salva imagem processada na pasta definida
-
-        os.remove(captcha_file) # remove arquivo de captcha utilizado
-
-    print("Processamento de imagens finalizado")
-
-# Metodo para extrair cada caractere dos captchas processados e salvar em sua pasta especifica
-def ExtrairCaracteres():
-
-    print("Extração de caracteres iniciado...")
-    
-    # Para cada arquivo de imagem na pasta dos captchas processados
-    image_files = glob.glob(os.path.join(PASTA_CAPTCHAS_PROCESSADOS, "*"))
-
-    for (i, captcha_file) in enumerate(image_files):
-
-        # Adquire uma lista com cada caracte do captcha
-        nome_arquivo = captcha_file.split("\\")[2]
-        nome_arquivo = nome_arquivo.split(".")[0]
-        caracteres = list(nome_arquivo)
-
-        # Abre a imagem em escala de cinza e aplica uma binarização (necessario para usar função de contorno)
-        img = cv2.imread(captcha_file, cv2.IMREAD_GRAYSCALE)
-        ret, img_processada = cv2.threshold(img, 127, 255, 0)
+        img_processada2 = RemoveGroupedPixels(img_processada, 50)
 
         # Adquire os contornos da imagem
-        img_processada = img_processada.astype(np.uint8)
-        contours, _ = cv2.findContours(img_processada.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        img_processada2 = img_processada2.astype(np.uint8)
+        contours, _ = cv2.findContours(img_processada2.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
         # Adquire as regiões da imagem onde estão os contornos
-        # Se o contorno estiver na borda da imagem ou muito proximo a ela, o desconsidera como contorno de algum 
-        # caractere
+        # Se o contorno estiver na borda da imagem ou muito proximo a ela, o desconsidera como contorno de algum caractere
         char_regions = []
         for contour in contours:
             (x, y, width, height) = cv2.boundingRect(contour)
             if width < 10 or height < 10:
                 char_regions = []
             char_regions.append((x, y, width, height))
-
+           
         # Se foi encontrado mais regiões que caracteres, algo deu errado. Nesse caso desconsideramos essa imagem
-        char_images = []
         if len(caracteres) > 0:
             if len(char_regions) != len(caracteres):
-                #print("Não foi possivel extrair os caracteres da imagem ", captcha_file)
+                print("Não foi possivel extrair os caracteres da imagem ", captcha_file)
                 os.remove(captcha_file)
                 continue
         
-
         # ordena os contornos obtidos da esquerda para a direita
+        char_images = []
         char_images = sorted(char_regions, key=lambda x: x[0])
 
         # Para cada contorno obtido, adquire a imagem correspondente
@@ -204,9 +164,9 @@ def ExtrairCaracteres():
             cv2.imwrite(PASTA_CARACTERES + "\\" + str(save_folder) + "\\" + str(img_name)+".png", caractere)  
             i+=1
 
-        os.remove(captcha_file) # Remove arquivo de captcha utilizado
-    
-    print("Extração de caracteres finalizado...")
+        os.remove(captcha_file) # remove arquivo de captcha utilizado
+
+    print("Processamento de imagens finalizado")
 
 # Obtem o primeiro nome disponivel para salvar um arquivo de imagem em uma pasta onde os arquivo são enumerados
 # em ordem crescente : Ex. 1.png, 2.png, ...
@@ -230,4 +190,3 @@ def RemoveGroupedPixels(img, group_size):
 
 PegarCaptchas()
 ProcessarCaptchas()
-ExtrairCaracteres()
